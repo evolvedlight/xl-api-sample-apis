a97de06634f1825f4e90ef5958166fc41587d43a
Stephen Brown <steve@evolvedlight.co.uk>
2019-08-31 19:43:18 +0200

    Add OpenApi3

--- basic_auth/Startup.cs
+++ basic_auth/Startup.cs
@@ -1,17 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using basic_auth.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Authentication;
+using NSwag.Generation.Processors.Security;
 
 namespace basic_auth
 {
@@ -33,6 +27,23 @@ namespace basic_auth
 
             // configure DI for application services
             services.AddScoped<IUserService, UserService>();
+
+            services.AddOpenApiDocument(document => {
+                document.Title = "Sample API with Basic Authentication";
+                document.Description = "This is a sample OpenApi3 API with Basic Authentication. Try it with test/testpass";
+                document.AddSecurity("basic", new NSwag.OpenApiSecurityScheme() {
+                    Type = NSwag.OpenApiSecuritySchemeType.Http,
+                    Scheme = "basic"
+                });
+                document.OperationProcessors.Add(
+                    new AspNetCoreOperationSecurityScopeProcessor("basic"));
+
+                document.PostProcess = d => d.Info.Contact = new NSwag.OpenApiContact() { 
+                    Name = "XlApi Demo Api Developers",
+                    Url = "https://xlapi.app",
+                    Email = "apiteam@xlapi.app" 
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +64,9 @@ namespace basic_auth
             app.UseAuthentication();
 
             app.UseAuthorization();
+
+            app.UseOpenApi(); // serve OpenAPI/Swagger documents
+            app.UseSwaggerUi3(); // serve Swagger UI
 
             app.UseEndpoints(endpoints =>
             {
